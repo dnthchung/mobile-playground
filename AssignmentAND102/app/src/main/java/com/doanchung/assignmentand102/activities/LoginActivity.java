bc7a690690a6908be341b4f47c6ef24f5c97ab55
chunChu <doanthanhchung03nb@gmail.com>
2024-05-17 13:40:02 +0700

    feat: fix analog reset password
    - thay đổi Linear thành cardview cho dialog quên mk
    - thêm :         alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
    để chỉnh bg của card view = với shape của background

--- AssignmentAND102/app/src/main/java/com/doanchung/assignmentand102/activities/LoginActivity.java
+++ AssignmentAND102/app/src/main/java/com/doanchung/assignmentand102/activities/LoginActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -91,6 +93,7 @@ public class LoginActivity extends AppCompatActivity {
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         alertDialog.setCancelable(false);
 
